c958fad0598953bac7513e8b82bd841ddfa2f6bb
Thabata356 <thabatav7@gmail.com>
2025-09-27 19:25:57 -0300

    :sparkles:Feat:Continuar o .xaml do CadastroView e LoginView.

--- ProjetoFinal-3bimestre-PAMII/ProjetoFinal/MauiProgram.cs
+++ ProjetoFinal-3bimestre-PAMII/ProjetoFinal/MauiProgram.cs
@@ -13,10 +13,24 @@ namespace ProjetoFinal
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Montserrat-Regular.ttf", "MontserratRegular");
+                    fonts.AddFont("Montserrat-SemiBold.ttf", "MontserratSemiBold");
                 });
 
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("Borderless", (handler, view) =>
+            {
+#if ANDROID
+                handler.PlatformView.Background = null;
+                handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#elif IOS || MACCATALYST
+                            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                            handler.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+#endif
+            });
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
